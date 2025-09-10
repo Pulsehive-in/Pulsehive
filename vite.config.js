00d1bd78ddddc26d21924fc59937434baafeb134
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-10 16:43:25 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // No alias needed for react-router
-  // No need to optimize 'react-router'
-  optimizeDeps: {
-    include: ['react-router-dom'] // only this is needed
-  }
 });
