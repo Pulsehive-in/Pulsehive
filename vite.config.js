205bbc462bcdd68ce5f1f87f8d1f2182695ee306
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-10 16:27:49 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Ensures Vite resolves react-router from node_modules
+      'react-router': path.resolve(__dirname, 'node_modules/react-router')
+    }
+  }
+});
