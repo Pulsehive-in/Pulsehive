ab1d2909b18692bad26bc7499cf4e01e3200f05b
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-30 14:46:14 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import {Navbar} from "./components/ui/resizable-navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Outlet />
+      <div className="w-[100vw] h-[100vh] overflow-hidden">
+        <Outlet />
+      </div>
     </>
   );
 }
